12f94816ce82e2a23d96f03b9333ce3289ab1dc6
omergunes <omeralpergunes@gmail.com>
2022-10-18 22:36:21 +0300

    Test 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("TES1");
+        System.out.println("TES12");
     }
 }
