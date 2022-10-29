0fe5d0fbc01123a780cd233877aec3561b4aa572
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-10-29 22:52:20 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("TES12");
-        System.out.println("Hello");
-    }
-}
