d443d2c77bdaf817106d38a79c3347e9104ce09c
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-10-29 22:47:15 +0300

    Delete TodoList4-5 directory

--- TodoList4-5/TodoList/src/main/java/com/omergunes/todolist/repo/MondayRepo.java
+++
@@ -1,8 +0,0 @@
-package com.omergunes.todolist.repo;
-
-import com.omergunes.todolist.model.Monday;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MondayRepo extends JpaRepository<Monday, Long> {
-
-}
